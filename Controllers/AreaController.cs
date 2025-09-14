473912e82bef1c86236ddd910376264ad53ba6be
Gabriel Amara <gabriel.amara@hotmail.com>
2025-09-14 17:38:59 -0300

    Testes na memoria + Endpoint GetAll base

---
+++ Controllers/AreaController.cs
@@ -0,0 +1,24 @@
+﻿using Case.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Case.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AreaController : Controller
+    {
+
+        private static List<Area> listaArea = new List<Area>()
+        {
+            new Area() {Id = 1, Nome = "TI", Descricao = "Area de TI", Status = true, Responsavel="Gabriel"},
+            new Area() {Id = 2, Nome = "Financeiro", Descricao = "Area do Financeiro", Status = true, Responsavel="Julia"},
+            new Area() {Id = 3, Nome = "RH", Descricao = "Area do RH", Status = true, Responsavel="Rodrigo"}
+        };
+
+        [HttpGet]
+        public ActionResult<List<Area>> GetAll()
+        {
+            return Ok(listaArea);
+        }
+    }
+}
---
+++ Controllers/ProcessoController.cs
@@ -0,0 +1,24 @@
+﻿using Case.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Case.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProcessoController : Controller
+    {
+        private static List<Processo> listaProcesso = new List<Processo>()
+        {
+            new Processo() { Id = 1,Nome = "Infra", Descricao = "Nada",IdArea = 1, Status = true},
+            new Processo() { Id = 2,Nome = "Lista Pc", Descricao = "Filho de Infra",IdArea = 1, Status = true, IdPai = 1},
+            new Processo() { Id = 3,Nome = "Contas", Descricao = "Nada",IdArea = 2, Status = true},
+            new Processo() { Id = 4,Nome = "Pendencias", Descricao = "Filho de Contas",IdArea = 2, Status = true, IdPai = 3},
+        };
+
+        [HttpGet]
+        public ActionResult<List<Processo>> GetAll()
+        {
+            return Ok(listaProcesso);
+        }
+    }
+}
