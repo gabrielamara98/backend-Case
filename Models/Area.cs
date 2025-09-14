03785314ab0c1a1d2e5d7da83f412b94545363e7
Gabriel Amara <gabriel.amara@hotmail.com>
2025-09-14 17:12:56 -0300

    Criação de Modelos Base

---
+++ Models/Area.cs
@@ -0,0 +1,18 @@
+﻿namespace Case.Models
+{
+    public class Area
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Descricao{ get; set; }
+
+        public string Responsavel { get; set; }
+
+        public bool Status { get; set; }
+
+        public DateTime Criado { get; set; }
+
+        public DateTime Atualizado { get; set; }
+    }
+}
---
+++ Models/Processo.cs
@@ -0,0 +1,24 @@
+﻿namespace Case.Models
+{
+    public class Processo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public int IdPai { get; set; }
+
+        public int IdArea { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Responsavel { get; set; }
+
+        public bool Status { get; set; }
+
+        public DateTime Criado { get; set; }
+
+        public DateTime Atualizado { get; set; }
+
+
+    }
+}
